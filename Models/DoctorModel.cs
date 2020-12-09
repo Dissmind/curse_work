a6fe15929f955c51ced9dbb8b4aa4e61faccfc8c
Dissmind <dissmind@gmail.com>
2020-12-09 20:16:53 +0200

    [ADD] models init

---
+++ Models/DoctorModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace curse_work.Models
+{
+    class DoctorModel
+    {
+        public int Id;
+        public string Name;
+        public string Type;
+    }
+}
---
+++ Models/MedWatchModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace curse_work.Models
+{
+    class MedWatchModel
+    {
+        public int Id;
+        public DoctorModel Doctor;
+        public string Age;
+        public string Description;
+
+        public MedWatchModel(int id, DoctorModel doctor, string age, string description)
+        {
+            Id = id;
+            Doctor = doctor;
+            Age = age;
+            Description = description;
+        }
+    }
+}
