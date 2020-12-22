b04a53b6d0a81ed5dc0dfe5dcec3917bac5ff7b6
Dissmind <dissmind@gmail.com>
2020-12-22 11:21:55 +0200

    [ADD]: getAll from DoctorRepository

--- Repository/DoctorsRepository.cs
+++ Repository/DoctorsRepository.cs
@@ -7,11 +7,38 @@ using System.Text;
 
 namespace curse_work.Repository
 {
-    class DoctorsRepository
+    public class DoctorsRepository
     {
         public static ICollection GetAll()
         {
-            return new List<int>();
+            var result = new List<DoctorModel>();
+
+            var db = new DB();
+
+            string table = "doctors";
+            string query = $"SELECT * FROM {table}";
+
+            db.OpenConnection();
+
+            MySqlCommand command = new MySqlCommand(query, db.GetConnection());
+
+            MySqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                var data = new DoctorModel
+                    (
+                        Int32.Parse(reader.GetValue(0).ToString()),
+                        reader.GetValue(1).ToString(),
+                        reader.GetValue(2).ToString()
+                    );
+
+                result.Add(data);
+            }
+
+            db.CloseConnection();
+
+            return result;
         }
 
         public static void Add(DoctorModel data)
